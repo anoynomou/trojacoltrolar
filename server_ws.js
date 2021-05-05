c9f366a13b6b0fd435849e1c932a1c464cc0b34d
galib <wsite079@gmail.com>
2021-05-05 15:49:23 +0600

    djidj

--- server_ws.js
+++ server_ws.js
@@ -3,22 +3,44 @@ var PORT = process.env.PORT || 3000;
 var Server =new Webserver.Server({port:PORT},()=>{})
 
 
+
+
 Server.on("connection",(ws)=>{
 
+
+
+
+// when message receved
 ws.on('message',(data)=>{
+
 Server.clients.forEach((client)=>{
-client.send(data);
-console.log(client);
+
+// if connection not == my connection
+// send all user data  except me!
+if(client != ws)
+
+client.send(data);    
+
 })
+
 })
 
 
+ws.on('close',()=>{})
+
 })
+
+
+
 
 
 Server.on('error',(e)=>{
-console.log(e);
+
 })
+
+
+
+
 Server.on('listening',()=>{
 
 })
