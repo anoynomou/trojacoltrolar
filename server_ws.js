8c62b2f96b66f1b60e3d51e9b0f82910e98ef34e
galib <wsite079@gmail.com>
2021-05-08 14:17:23 +0600

    dara

--- server_ws.js
+++ server_ws.js
@@ -1,6 +1,6 @@
 var Webserver = require('ws');
 var PORT = process.env.PORT || 3000;
-var Server =new Webserver.Server({port:PORT},()=>{})
+var Server =new Webserver.Server({port:PORT},()=>{console.log("Server is runing on port : "+ PORT)});
 
 
 
@@ -9,24 +9,35 @@ Server.on("connection",(ws)=>{
 
 
 
+console.log("clint connected")
 
 // when message receved
 ws.on('message',(data)=>{
+
 
 Server.clients.forEach((client)=>{
 
 // if connection not == my connection
 // send all user data  except me!
-if(client != ws)
+if(client != ws && client.readyState == ws.OPEN)
 
 client.send(data);    
-
+console.log("dat sended")
 })
 
 })
 
 
-ws.on('close',()=>{})
+ws.on('close',()=>{
+console.log("clint disconnect")
+
+})
+
+
+
+
+
+
 
 })
 
